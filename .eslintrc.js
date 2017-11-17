c9b8539b8b51aff39d60fae36e928937c654c073
Ilgiz Mavlyaviev <ilgiz.personal@gmail.com>
2017-11-18 01:12:10 +0300

    [ADD] Remove eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,7 @@ module.exports = {
         "semi": [2, "never"],
         "camelcase": [0, { properties: 'never' }],
         "import/extensions": [0, "never"],
-        "import/prefer-default-export": "off"
+        "import/prefer-default-export": "off",
+        "jsx-a11y/click-events-have-key-events": "off"
     }
 };
